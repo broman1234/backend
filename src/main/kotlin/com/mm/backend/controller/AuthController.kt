da08641831df412ef0ea7eb9f67b0c480a8a0fcd
MOMAN11 <thoughtworks.mo@extaccount.com>
2023-09-02 10:28:57 +0800

    delete unused import

--- src/main/kotlin/com/mm/backend/controller/AuthController.kt
+++ src/main/kotlin/com/mm/backend/controller/AuthController.kt
@@ -12,7 +12,6 @@ import com.mm.backend.service.UserService
 import mu.KLogging
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.web.bind.annotation.*
